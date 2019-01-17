d9e733ddd07de589d8231dfebf7fa95de13adc05
Your Name <you@example.com>
2019-01-18 07:55:02 +0800

    firstGit

---
+++ src/HelloGit.java
@@ -0,0 +1,10 @@
+/**
+ * @Author：万腾
+ * @Description：暂无
+ * @Date：创建于上午 7:42
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("HelloGit");
+    }
+}
